1ec854fb83272b8dc6d49b56b43be5ac0620d623
Adudin <85031696+adudon10@users.noreply.github.com>
2024-05-21 17:04:57 +0700

    Create adudin.js

---
+++ adudin.js
@@ -0,0 +1,4 @@
+var app = new Vue({ 
+    el: '#app',
+});
+document.write( '<center><p><small><small>made with ❤ by <a href=\'Dudu Saepudin' class=\'text-danger\' target=\'blank\'>Dudu Saepudin</a> <br/><marquee scrollamount="05">Special Thanks to Team dev : <a href=\'AVIK Technology\' class=\'text-danger\' target=\'blank\'> Creative - <a href=\'Dudu Saepudin\' class=\'text-danger\' target=\'blank\'>Vika Sugih Rahayu</a> - source code free download .</small></small></marquee></p></center>' );
